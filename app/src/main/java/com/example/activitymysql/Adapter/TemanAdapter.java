4b2ed1b1fdd77c2141053d5b88ab824db8a4a8c1
wachidmuslih <wachidmslh@gmail.com>
2022-04-26 13:00:00 +0700

    update

---
+++ app/src/main/java/com/example/activitymysql/Adapter/TemanAdapter.java
@@ -0,0 +1,91 @@
+package com.example.activitymysql.Adapter;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.activitymysql.Database.Teman;
+import com.example.activitymysql.MainActivity;
+import com.example.activitymysql.R;
+
+import java.util.ArrayList;
+
+public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder> implements Filterable {
+
+    private ArrayList<Teman> listdata;
+    //private final TemanAdapterListener listener;
+
+    public TemanAdapter(ArrayList<Teman> listdata) {
+        this.listdata = listdata;
+    }
+
+    public TemanAdapter(ArrayList<Teman> temanArrayList, MainActivity mainActivity) {
+
+    }
+
+    @Override
+    public Filter getFilter() {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    public TemanViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInf = LayoutInflater.from(parent.getContext());
+        View view = layoutInf.inflate(R.layout.row_data_teman,parent,false);
+        return new TemanViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TemanViewHolder holder, int position) {
+        String nm,tlp, id;
+
+        id = listdata.get(position).getId();
+        nm = listdata.get(position).getNama();
+        tlp = listdata.get(position).getTelpon();
+
+        holder.namaTxt.setTextColor(Color.BLUE);
+        holder.namaTxt.setTextSize(20);
+        holder.namaTxt.setText(nm);
+        holder.telponTxt.setText(tlp);
+
+        holder.cardku.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return (listdata != null)?listdata.size() : 0;
+    }
+
+    public class TemanViewHolder extends RecyclerView.ViewHolder {
+        private CardView cardku;
+        private TextView namaTxt, telponTxt;
+
+        public TemanViewHolder(View view) {
+            super(view);
+            cardku = (CardView) view.findViewById(R.id.kartuku);
+            namaTxt = (TextView) view.findViewById(R.id.textNama);
+            telponTxt = (TextView) view.findViewById(R.id.textTelpon);
+        }
+    }
+}
--- app/src/main/java/com/example/activitymysql/MainActivity.java
+++ app/src/main/java/com/example/activitymysql/MainActivity.java
@@ -1,14 +1,119 @@
 package com.example.activitymysql;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.activitymysql.Adapter.TemanAdapter;
+import com.example.activitymysql.Database.Teman;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton fab;
+    private Button btn;
+    private RecyclerView recyclerView;
+    private TemanAdapter adapter;
+    //untuk menyimpan data teman
+    private ArrayList<Teman> temanArrayList;
+
+    private static  final String TAG = MainActivity.class.getSimpleName();
+    private static String url_select = "http://10.0.2.2/umyTI2/bacateman.php";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_TELPON = "telpon";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        fab = findViewById(R.id.floatingBtn);
+        btn = findViewById(R.id.bHapus);
+
+        btn.setOnClickListener((v ->{
+            new AlertDialog.Builder(this).setMessage("Hapus semuanya?")
+                    .setPositiveButton("Hapus",(y, x)-> {
+                        Toast.makeText(this, "Hapus", Toast.LENGTH_SHORT).show();
+                    } )
+                    .setNegativeButton("Batal",(y, x)-> {
+                        Toast.makeText(this, "Batal", Toast.LENGTH_SHORT).show();
+                    }).show();
+        }));
+
+        BacaData();
+        adapter = new TemanAdapter(temanArrayList, this);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,TambahTeman.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void BacaData() {
+        temanArrayList.clear();
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+        JsonArrayRequest jArr = new JsonArrayRequest(url_select, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.d(TAG, response.toString());
+
+                //parsing json
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject obj = response.getJSONObject(i);
+
+                        Teman item = new Teman();
+
+                        item.setId(obj.getString(TAG_ID));
+                        item.setNama(obj.getString(TAG_NAMA));
+                        item.setTelpon(obj.getString(TAG_TELPON));
+
+                        //menambah item ke array
+                        temanArrayList.add(item);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                adapter.notifyDataSetChanged();
+
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: "+ error.getMessage());
+                error.printStackTrace();
+                Toast.makeText(MainActivity.this,"gagal", Toast.LENGTH_LONG).show();
+            }
+        });
+        requestQueue.add(jArr);
     }
 }

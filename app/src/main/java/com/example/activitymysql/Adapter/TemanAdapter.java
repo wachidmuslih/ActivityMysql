ab47129001f14417069d4f4c45b6e668584a0537
wachidmuslih <wachidmslh@gmail.com>
2022-05-21 20:43:34 +0700

    update delete and edit

--- app/src/main/java/com/example/activitymysql/Adapter/TemanAdapter.java
+++ app/src/main/java/com/example/activitymysql/Adapter/TemanAdapter.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,11 +20,21 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.example.activitymysql.AppController.AppController;
 import com.example.activitymysql.Database.Teman;
 import com.example.activitymysql.MainActivity;
 import com.example.activitymysql.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder> implements Filterable {
 
@@ -64,12 +75,97 @@ public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHol
         holder.namaTxt.setText(nm);
         holder.telponTxt.setText(tlp);
 
+
         holder.cardku.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
+                PopupMenu pm = new PopupMenu(v.getContext(), v);
+                pm.inflate(R.menu.popup1);
+
+                pm.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId())
+                        {
+                            case R.id.edit:
+                                Bundle bendel = new Bundle();
+
+                                bendel.putString("kunci1", id);
+                                bendel.putString("kunci2", nm);
+                                bendel.putString("kunci3", tlp);
+
+                                Intent inten = new Intent();
+                                inten.putExtras(bendel);
+                                v.getContext().startActivity(inten);
+                                break;
+
+                            case R.id.hapus:
+                                AlertDialog.Builder alertdb = new AlertDialog.Builder(v.getContext());
+                                alertdb.setTitle("Yakin " +nm+" akan dihapus?");
+                                alertdb. setMessage("Tekan Ya untuk menghapus");
+                                alertdb. setCancelable(false);
+                                alertdb. setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        HapusData(id);
+                                        Toast.makeText(v.getContext(), "Data "+id+" telah dihapus", Toast.LENGTH_LONG).show();
+                                        Intent intent = new Intent(v. getContext(), MainActivity.class);
+                                        v.getContext().startActivity(intent);
+                                    }
+                                });
+                                alertdb.setNegativeButton("Tidak", new DialogInterface.OnClickListener(){
+
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.cancel();
+                                    }
+                                });
+                                AlertDialog adlg = alertdb.create();
+                                adlg.show();
+                                break;
+                        }
+                        return true;
+                    }
+                });
+                pm.show();
                 return true;
             }
         });
+    }
+
+    private void HapusData(final String idx) {
+        String url_update = "http://10.0.2.2/umyTI/deletetm.php";
+        final String TAG = MainActivity.class.getSimpleName();
+        final String TAG_SUCCES = "success";
+        final int[] sukses = new int[1];
+
+        StringRequest stringReq = new StringRequest(Request. Method. POST, url_update, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG,"Respon: " + response.toString());
+                try{
+                    JSONObject jobj = new JSONObject(response);
+                    sukses [0] = jobj.getInt(TAG_SUCCES) ;
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log . e (TAG, "Error : "+ error. getMessage());
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams()  {
+                Map<String , String> params = new HashMap<>();
+                params.put("id", idx);
+                return params;
+            }
+        };
+        AppController.getInstance().addToRequestQueue(stringReq);
+
     }
 
     @Override
